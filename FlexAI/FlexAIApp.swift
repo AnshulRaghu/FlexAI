ca3cd8ec44dc51a317dd6917400fd61c2bcdf53e
hbhuta23 <he>
2025-04-05 14:32:42 -0400

    Initial Commit

---
+++ FlexAI/FlexAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexAIApp.swift
+//  FlexAI
+//
+//  Created by Aastha Jakasania on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FlexAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
